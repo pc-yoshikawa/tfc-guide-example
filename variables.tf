d13b33803c650ce3da311f298f97d79a1dce8a09
yskw516 <kou@yskw.info>
2024-06-19 19:21:25 +0900

    change default values for some variables

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@
 
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-west-2"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t3.nano"
 }
 
 variable "instance_name" {
