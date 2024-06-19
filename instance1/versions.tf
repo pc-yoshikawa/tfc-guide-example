4d60b2af279d69c9e75ef4fe907086776fc21572
yskw516 <kou@yskw.info>
2024-06-19 19:51:10 +0900

    Move files to instance1 directory

--- instance1/versions.tf
+++ instance1/versions.tf
@@ -2,6 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  backend "remote" {
+    organization = "pc-yoshikawa"
+
+    workspaces {
+      name = "tfc-guide-example"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
